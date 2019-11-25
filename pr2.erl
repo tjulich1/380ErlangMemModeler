5813cda22f20b4df1aecd27f68910395c906f5e5
Trent Julich <tjulich1@gmail.com>
2019-11-24 16:39:33 -0800

    final version.

--- pr2.erl
+++ pr2.erl
@@ -4,21 +4,19 @@
 -export([mymain/0]).
 -export([directoryAsString/1]).
 -export([blocksAsString/1]).
--export([removeFileDirTable/2]).
--export([createBlockTable/2]).
+-export([printMenu/0]).
 
--export([addFileDirTable/5]).
--export([printDirTable/1]).
--export([readUserSelection/0]).
-
+% Driver for the program. Used to initialize the directory and block
+% tables and begin interacting with the user.
 mymain() ->
     {TotalSize, BlockSize} = initBlockTable(),
     BlockTable = createBlockTable(TotalSize, BlockSize),
     DirectoryTable = [],
     loop(DirectoryTable, BlockTable, BlockSize).
 
-loop(DirectoryTable, BlockTable, BlockSize) ->
-    
+% Main loop of the program, prompts user for selection, and handles selection.
+% Continues looping until user force quits, or selects menu item 4.
+loop(DirectoryTable, BlockTable, BlockSize) -> 
     Choice = readUserSelection(),
     case Choice of
         add -> 
@@ -51,6 +49,7 @@ loop(DirectoryTable, BlockTable, BlockSize) ->
             loop(DirectoryTable, BlockTable, BlockSize)
     end.
 
+% Prompts the user for the name and size of a file to add to the model.
 addFilePrompt() ->
     io:format("Please enter the file name: "),
     FileName = readInput(1),
@@ -58,10 +57,12 @@ addFilePrompt() ->
     FileSize = readInput(0),
     {FileName, FileSize}.
 
+% Prompts the user for the name of a file to remove from model.
 removeFilePrompt() ->
     io:format("Enter name of file to remove: "),
     readInput(1).
 
+% Prints the possible menu selections to the shell.
 printMenu() -> 
     io:format("1 - Add File~n"),
     io:format("2 - Remove File~n"),
@@ -72,6 +73,8 @@ printMenu() ->
 %% Input Operations %%
 %%%%%%%%%%%%%%%%%%%%%%
 
+% Function used to handle a users selection of one of the menu items
+% printed to the shell.
 readUserSelection() ->
     printMenu(),
     io:format("Please enter selection: "), 
@@ -84,6 +87,9 @@ readUserSelection() ->
         _ -> bad_input
     end.
 
+% Function used to read input from the shell. If the option passed = 0,
+% the function will read in an integer. If the option passed = 1, the 
+% function will read in a string.
 readInput(Option) ->
     case Option of 
         0 -> 
@@ -100,14 +106,17 @@ readInput(Option) ->
 %% Directory Table Operations %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Function used to add a file with the given properties to the 
+% given directory table.
 addFileDirTable( ListOfFileTuples, Name, Size, Start, Len ) ->
     NewEntry = {Name, Size, Start, Len},
     ListOfFileTuples++[NewEntry].
 
+% Function used to remove a file with the given name from the 
+% given directory table.
 removeFileDirTable([], _) -> io:format("Cannot remove from empty list.~n");
 removeFileDirTable([H|T], Name) ->
     removeFileDirTableHelper([], H, T, Name).  
-
 removeFileDirTableHelper(Searched, {Name, X, Y, Z}, Unsearched, NameToRemove) -> 
     if
         Name =:= NameToRemove ->
@@ -116,28 +125,31 @@ removeFileDirTableHelper(Searched, {Name, X, Y, Z}, Unsearched, NameToRemove) ->
             removeFileDirTableHelper(Searched++[{Name, X, Y, Z}], hd(Unsearched), tl(Unsearched), NameToRemove)
     end.
 
+% Function used to print the given directory table to the shell.
 printDirTableHeader() ->
     io:format("-------------------------~n"),
     io:format("Directory Table~n"),
     io:format("-------------------------~n"),
     io:format("FileName       FileSize~n").
-
 printDirTable( [] ) -> ok;
 printDirTable([{FileName, Size, _, _}|T]) ->
     io:fwrite("~s              ~w ~n", [FileName, Size]),
     printDirTable(T).
 
-directoryAsString( ListOfFileTuples ) -> directoryAsStringHelper( ListOfFileTuples, "").
-
-directoryAsStringHelper( [], StringSoFar ) -> StringSoFar;
-directoryAsStringHelper( [{FileName, Size, Start, Len} | T], StringSoFar) ->
-    Temp = "["++FileName++", "++Size++", "++Start++", "++Len++"]",
-    directoryAsStringHelper(T, StringSoFar++Temp).
+% Function used to return the given list of file tuples as a string. 
+% returns a string of the format "{FileName1, FileSize1}...{FileNameN, FileSizeN}"
+directoryAsString( ListOfFileTuples ) -> 
+    directoryAsStringHelper(ListOfFileTuples).
+directoryAsStringHelper([]) -> "";
+directoryAsStringHelper([{FileName, FileSize, _, _} | T]) ->
+    "{"++FileName++", "++lists:flatten(io_lib:format("~p",[FileSize]))++"}"++directoryAsStringHelper(T).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Block Table Operations %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Calculates and returns the number of blocks required to store
+% the given file with blocks of the given size.
 calculateReqBlocks(FileSize, BlockSize) ->
     WholeBlocks = FileSize div BlockSize,
     if
@@ -147,6 +159,10 @@ calculateReqBlocks(FileSize, BlockSize) ->
             WholeBlocks
     end.
 
+% Takes the given block table and sets the "used" and "fragmented" values
+% to zero where 
+freeBlocks([], _, _, _) ->
+    [];
 freeBlocks([Head|Tail], 1, FileSize, BlockSize) ->
     if 
         FileSize =< 0 ->
@@ -157,6 +173,9 @@ freeBlocks([Head|Tail], 1, FileSize, BlockSize) ->
 freeBlocks(BlockTable, StartBlock, FileSize, BlockSize) ->
     freeBlocks(BlockTable, StartBlock - 1, FileSize, BlockSize).    
 
+% Function used to update the block table, filling in the used and fragmented
+% values. Takes in the total file size, as well as the size of each blocks,
+% along with the start index where the updating should start.
 updateBlockTable([], _, _, _) ->
     []; 
 updateBlockTable([{Used, Fragmented} | T], 1, FileSize, BlockSize) ->
@@ -174,8 +193,11 @@ updateBlockTable([{Used, Fragmented} | T], 1, FileSize, BlockSize) ->
 updateBlockTable([H|T], StartIndex, FileSize, BlockSize) ->
     [H]++updateBlockTable(T, StartIndex - 1, FileSize, BlockSize).
 
+% Function used to search for free blocks to store a file needing "ReqBlocks" 
+% blocks. Will return -1 if there is not enough room for it in the table, 
+% or else will return the index of the first block it was saved in.
 findFirstFreeBlock([], _, _) -> -1;
-findFirstFreeBlock([{Used, Fragmented}|T], ReqBlocks, Index) ->
+findFirstFreeBlock([{Used, _}|T], ReqBlocks, Index) ->
     case Used of
         0 -> 
             Success = findConsecutive(T, ReqBlocks - 1),
@@ -186,6 +208,8 @@ findFirstFreeBlock([{Used, Fragmented}|T], ReqBlocks, Index) ->
         _ -> findFirstFreeBlock(T, ReqBlocks, Index + 1)
     end.
 
+% Function to check consecutive blocks. Returns 'success' if consecutive blocks
+% are open, 'failure' otherwise.
 findConsecutive(_, 0) -> 
     success;
 findConsecutive([], _) ->
@@ -196,6 +220,7 @@ findConsecutive([{Used, _}|T], ReqBlocks) ->
         _ -> failure
     end.
 
+% Prompts user for total memory size, and size of each block.
 initBlockTable() ->
     io:format("Enter total size of memory model: "),
     TotalSize = readInput(0),
@@ -203,6 +228,8 @@ initBlockTable() ->
     BlockSize = readInput(0),
     {TotalSize, BlockSize}.
 
+% Creates and returns a list of tuples with ceil(TotalSize/BlockSize) 
+% number of tuples representing blocks.
 createBlockTable(TotalSize, BlockSize) ->
     Temp = TotalSize div BlockSize,
     if
@@ -217,15 +244,24 @@ createBlockTableHelper(List, 0) -> List;
 createBlockTableHelper(List, NumBlocksRem) ->
     createBlockTableHelper(List++[{0,0}], NumBlocksRem-1).
     
+% Prints the given block table to the shell.
 printBlockTable([]) -> ok;
 printBlockTable([{Free, Fragmented}|T]) ->
     io:format("~w       ~w ~n", [Free, Fragmented]),
     printBlockTable(T).
 
+% Prints a header for a block table to the shell.
 printBlockTableHeader() ->
     io:format("------------------~n"),
     io:format("Block Table ~n"),
     io:format("------------------~n"),
     io:format("Free    Fragmented ~n").
 
-blocksAsString( ListOfBlockTuples ) -> "TODO".
+blocksAsString( ListOfBlockTuples ) -> 
+    io:format("Used    Fragmented~n"),
+    blocksAsStringHelper(ListOfBlockTuples).
+
+blocksAsStringHelper([]) -> ok;
+blocksAsStringHelper([{Used, Fragmented}|T]) ->
+    io:format("~w       ~w~n", [Used, Fragmented]),
+    blocksAsStringHelper(T).
